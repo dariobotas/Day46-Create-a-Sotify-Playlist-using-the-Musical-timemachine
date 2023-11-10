b59f5b4f5480b0786b073efa14999d3b9cc49f48
dariobotas <dario.botas@gmail.com>
2023-11-10 09:29:49 +0000

    Spotify functiona

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
+import spotify_playlist as sp
+
 URL = "https://www.billboard.com/charts/hot-100/"
 
 if __name__ == '__main__':
-  year_to_travel = input("Wich year do you want to travel to? Type the date in this format YYYY-MM-DD:\n")
+  """ Step 2"""
+  sp.run()
+  
+  """ Step 1"""
+  #year_to_travel = input("Wich year do you want to travel to? Type the date in this format YYYY-MM-DD:\n")
 
-  url_scrap = f"{URL}{year_to_travel}"
+  url_scrap = f"{URL}2000-08-12"#{year_to_travel}"
   response = requests.get(url_scrap).text
   soup = BeautifulSoup(response, "html.parser")
  
@@ -25,3 +31,5 @@ if __name__ == '__main__':
   """ Print Songs / Artists List """
   for i in range(len(songs)):
     print(f"{i+1}. Song: {songs[i]} / Artist: {artists[i]}")
+
+
---
+++ spotify_playlist.py
@@ -0,0 +1,22 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import os
+
+SPOTIFY_CLIENT_ID = os.environ["SPOTIFY_CLIENT_ID"]
+SPOTIFY_CLIENT_SECRET = os.environ["SPOTIFY_CLIENT_SECRET"]
+SPOTIPY_REDIRECT_URI = os.environ["SPOTIFY_REDIRECT_URI"]
+
+def run():
+  spotify = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+      client_id=SPOTIFY_CLIENT_ID,
+      client_secret=SPOTIFY_CLIENT_SECRET,
+      redirect_uri="http://example.com",
+      scope="playlist-modify-private",
+      show_dialog=True,
+      cache_path="token.txt",
+      username="Dario", 
+    ))
+  results = spotify.current_user()
+  user_id = results["id"]
+  print(f"User is: {user_id}")
